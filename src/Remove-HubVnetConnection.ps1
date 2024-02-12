1da6d6d3ec9cff8da4cbb49eae1986bdcf96854b
Jonathan Ryan <realjonathanryan@outlook.com>
2024-02-11 22:20:04 -0800

    update

--- src/Remove-HubVnetConnection.ps1
+++ src/Remove-HubVnetConnection.ps1
@@ -14,28 +14,4 @@ If (-NOT(Get-Module -ListAvailable Az.Network)) {
     Install-Module Az.Network -Scope CurrentUser -Force
 }
 
-$connectionName = "AzureRunAsConnection"
-try
-{
-    # Get the connection "AzureRunAsConnection "
-    $servicePrincipalConnection=Get-AutomationConnection -Name $connectionName         
-
-    "Logging in to Azure..."
-    Add-AzureRmAccount `
-        -ServicePrincipal `
-        -TenantId $servicePrincipalConnection.TenantId `
-        -ApplicationId $servicePrincipalConnection.ApplicationId `
-        -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint 
-}
-catch {
-    if (!$servicePrincipalConnection)
-    {
-        $ErrorMessage = "Connection $connectionName not found."
-        throw $ErrorMessage
-    } else{
-        Write-Error -Message $_.Exception
-        throw $_.Exception
-    }
-}
-
 Get-AzVirtualHubVnetConnection -ResourceGroupName $resourceGroup -VirtualHubName $virtualHubName -Name $ConnectionName | Remove-AzVirtualHubVnetConnection
