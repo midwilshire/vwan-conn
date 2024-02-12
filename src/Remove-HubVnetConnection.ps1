64b71bc4a85d2be2b003563786579e33143f1a38
Jonathan Ryan <realjonathanryan@outlook.com>
2024-02-11 22:09:46 -0800

    Force

--- src/Remove-HubVnetConnection.ps1
+++ src/Remove-HubVnetConnection.ps1
@@ -11,7 +11,7 @@ $virtualHubName = $VirtualWanHubResourceId.Split("/")[8]
 If (-NOT(Get-Module -ListAvailable Az.Network)) {
     Write-Warning "This script requires the Az.Network module."
 
-    Install-Module Az.Network -Scope CurrentUser
+    Install-Module Az.Network -Scope CurrentUser -Force
 }
 
 Get-AzVirtualHubVnetConnection -ResourceGroupName $resourceGroup -VirtualHubName $virtualHubName -Name $ConnectionName | Remove-AzVirtualHubVnetConnection
